b180ed102d0f8d3452af873561573e36d2d3d6f1
vsavenkov <vadim.savenkov@gmail.com>
2019-10-26 03:25:39 +0200

    Update setup.py
    pip >=v10

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ Created on Dec 2, 2015
 '''
 
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements("requirements.txt", session=False)
